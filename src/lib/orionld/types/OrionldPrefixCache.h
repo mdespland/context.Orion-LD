81c19624383bc9c2fdd7cf4fb513e5a4826ed455
Ken Zangelin <kzangeli@gmail.com>
2019-11-15 19:44:26 +0100

    Copyright headers

--- src/lib/orionld/types/OrionldPrefixCache.h
+++ src/lib/orionld/types/OrionldPrefixCache.h
@@ -3,25 +3,25 @@
 
 /*
 *
-* Copyright 2019 Telefonica Investigacion y Desarrollo, S.A.U
+* Copyright 2019 FIWARE Foundation e.V.
 *
-* This file is part of Orion Context Broker.
+* This file is part of Orion-LD Context Broker.
 *
-* Orion Context Broker is free software: you can redistribute it and/or
+* Orion-LD Context Broker is free software: you can redistribute it and/or
 * modify it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
-* Orion Context Broker is distributed in the hope that it will be useful,
+* Orion-LD Context Broker is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 * General Public License for more details.
 *
 * You should have received a copy of the GNU Affero General Public License
-* along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+* along with Orion-LD Context Broker. If not, see http://www.gnu.org/licenses/.
 *
 * For those usages not covered by this license please contact with
-* iot_support at tid dot es
+* orionld at fiware dot org
 *
 * Author: Ken Zangelin
 */
@@ -34,7 +34,8 @@
 //
 //   When prefixes are used, normally the very same, or very few prefixes are used.
 //   So, it makes sense to implement a cache for the prefix lookups.
-//   A simple vector of 10 key-values are used for this, as normal use-cases don't use more than just a few prefixes.
+//   A simple vector of 10 key-values are used for this, as normal use-cases don't use
+//   more than just a few prefixes.
 //   And, the smaller the cache, the quicker the lookups (especially the no-hits).
 //
 
